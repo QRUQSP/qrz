a843aee5e96961937bd97d0e40fba60e0c124fe3
Andrew Rivett <veggiefrog@gmail.com>
2019-08-21 18:44:34 +0000

    Added help information

--- hooks/uiSettings.php
+++ hooks/uiSettings.php
@@ -27,6 +27,7 @@ function qruqsp_qrz_hooks_uiSettings(&$ciniki, $tnid, $args) {
         $menu_item = array(
             'priority'=>5000,
             'label'=>'QRZ',
+            'helpcontent'=>'Lookup callsigns.',
             'edit'=>array('app'=>'qruqsp.qrz.main'),
             );
         $rsp['menu_items'][] = $menu_item;
